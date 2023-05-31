6197fd32ccee8fc5ac6ab6e9416f2d1febf7dbfe
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-05-31 18:57:02 +0530

    Add files via upload

---
+++ day8.c1.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+ int main(){
+ 	
+ 	int a,b
+ 	 
+	printf("Enter velue of a :");
+ 	 scanf("%d,a");
+ 	
+ 	printf("Enter velue of b : ");
+ 	 scanf("%d,b");
+ 	
+ 	if (a>b){
+	  
+ 	printf("a is gretarthen b :"); 
+ 	
+ 	
+ }
+ 	
+ 	easle  (b<a){
+	 
+ 	printf("a is gretarthen b :");
+ 	
+ 	return 0;
+ }
