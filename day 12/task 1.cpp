c2ccad499d33a422d0e396a709c180a93a30001e
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-06-19 18:40:05 +0530

    Add files via upload

---
+++ day 12/task 1.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+ int main(){
+
+  int i =10;
+   int n;
+
+
+
+	 printf("Enter value :");
+	 scanf("%d",&n);
+
+     while (i>=1){
+
+
+        printf("%d\n",i);
+        i--;
+
+
+
+
+	 }
+
+
+
+
+
+
+
+
+
+
+
+
+  return 0;
+
+ }
