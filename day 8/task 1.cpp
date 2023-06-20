9aeeec4d557828a05027d93e689bbe77f22a9166
jayesh141827 <134709268+jayesh141827@users.noreply.github.com>
2023-06-20 19:35:34 +0530

    Add files via upload

---
+++ day 8/task 1.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+
+	int a ;
+	int b ;
+     printf("Enter velue of a :");
+     scanf("%d",&a);
+
+
+     printf("Enter velue of b :");
+     scanf("%d",&b);
+
+	if(a>b){
+
+	 printf("a is greterthen b :");	
+
+
+	}
+
+	else {
+
+		printf("a is lessthen b :");
+	}
+
+
+
+
+
+}
